07823e9a5c1f42dfb58035c57140a7f40fa46dbc
Gabriel Kaffka <kaffka@gmail.com>
2018-07-14 19:37:11 +0000

    Replaces old routes with day route

--- app/routes/sun_routes.js
+++ app/routes/sun_routes.js
@@ -1,20 +1,9 @@
 var ObjectID = require('mongodb').ObjectID;
 
 module.exports = function(app, db) {
- 
-    app.post('/sunrise', (req, res) => {
-        const time = { sunrise: req.body.hour};
-        db.collection('sun').insert(time, (err, result) => {
-          if (err) { 
-            res.send({ 'error': 'An error has occurred' }); 
-          } else {
-            res.send(result.ops[0]);
-          }
-        });
-      });
-
-      app.post('/sunset', (req, res) => {
-        const time = { sunrset: req.body.hour};
+      
+      app.post('/day', (req, res) => {
+        const time = { sunrise: req.body.sunrise, sunrset: req.body.sunset};
         db.collection('sun').insert(time, (err, result) => {
           if (err) { 
             res.send({ 'error': 'An error has occurred' }); 
@@ -24,10 +13,10 @@ module.exports = function(app, db) {
         });
       });  
 
-      app.put('/sunset', (req, res) => {
-        const id = '5b4a2022970fa9042b9af188';
+      app.put('/day', (req, res) => {
+        const id = '5b4a4f47e74e3f0cb4fa8dbb';
         const details = { '_id': new ObjectID(id) };
-        const time = { sunset: req.body.hour };
+        const time = { sunrise: req.body.sunrise,  sunset: req.body.sunset };
         db.collection('sun').update(details, time, (err, result) => {
           if (err) {
               res.send({'error':'An error has occurred'});
@@ -37,32 +26,8 @@ module.exports = function(app, db) {
         });
       });
 
-      app.put('/sunrise', (req, res) => {
-        const id = '5b4a2019970fa9042b9af187';
-        const details = { '_id': new ObjectID(id) };
-        const time = { sunrise: req.body.hour };
-        db.collection('sun').update(details, time, (err, result) => {
-          if (err) {
-              res.send({'error':'An error has occurred'});
-          } else {
-              res.send(time);
-          } 
-        });
-      });
-
-      app.get('/sunrise', (req, res) => {
-        const details = { '_id': new ObjectID('5b4a2019970fa9042b9af187') };
-        db.collection('sun').findOne(details, (err, item) => {
-            if (err) {
-              res.send({'error':'An error has occurred'});
-            } else {
-              res.send(item);
-            }
-          });
-      });
-
-      app.get('/sunset', (req, res) => {
-        const details = { '_id': new ObjectID('5b4a2022970fa9042b9af188') };
+      app.get('/day', (req, res) => {
+        const details = { '_id': new ObjectID('5b4a4f47e74e3f0cb4fa8dbb') };
         db.collection('sun').findOne(details, (err, item) => {
             if (err) {
               res.send({'error':'An error has occurred'});
