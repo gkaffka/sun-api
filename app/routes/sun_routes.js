0108c7aacdcbec9d841c8dbdf912d5643e3923f2
Gabriel Kaffka <kaffka@gmail.com>
2018-07-28 18:03:23 +0000

    Creates day endpoint

--- app/routes/sun_routes.js
+++ app/routes/sun_routes.js
@@ -1,10 +1,15 @@
 const dayController = require("../controllers/day_controller");
+const temperatureController = require("../controllers/temperature_controller");
 
 module.exports = function(app) {
 
-      app.post('/day', dayController.post);  
+      app.post('/day', dayController.create);  
 
-      app.put('/day', dayController.put);
+      app.put('/day', dayController.update);
 
-      app.get('/day', dayController.get);
+      app.get('/day', dayController.fetch);
+
+      app.post('/temperature', temperatureController.create);  
+      
+      app.get('/temperature', temperatureController.fetch);
 };
